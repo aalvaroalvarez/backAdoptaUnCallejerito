bd3227b41d961afb571089c59a0efa373c74f7c0
aalvaroalvarez <alvanalar@gmail.com>
2021-07-31 13:32:08 -0500

    Comentar msj error

--- server.js
+++ server.js
@@ -50,14 +50,14 @@ const server = app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
 
-// 404 Error
-app.use((req, res, next) => {
-  next(404);
-  /* next(createError(404)); */
-});
+// // 404 Error
+// app.use((req, res, next) => {
+//   next(404);
+//   /* next(createError(404)); */
+// });
 
-app.use(function (err, req, res, next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
-});
+// app.use(function (err, req, res, next) {
+//   console.error(err.message);
+//   if (!err.statusCode) err.statusCode = 500;
+//   res.status(err.statusCode).send(err.message);
+// });
